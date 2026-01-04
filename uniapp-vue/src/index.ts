4b6ee333a30fa442dd78ad46a198e934c261a59f
alamhubb <alamhubb@163.com>
2026-01-05 00:56:31 +0800

    工厂不做处理

--- uniapp-vue/src/index.ts
+++ uniapp-vue/src/index.ts
@@ -12,15 +12,6 @@ export {
     cleanupEventHandlers
 } from './src/h'
 
-// UniApp 编译产物可能需要的辅助函数
-export function t(value: any): string {
-    return String(value ?? '')
-}
-
-export function o(handler: Function): Function {
-    return handler
-}
-
 // 版本信息
 const VERSION = '5.0.0'
 console.log(`[uniapp-vue] v${VERSION} - getApp().globalData 模式`)
