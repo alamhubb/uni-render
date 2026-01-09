1e5590ecc81b7e546d61741a37eb50821318a9ab
alamhubb <alamhubb@163.com>
2026-01-09 22:00:52 +0800

    1

--- my-vue3-project/src/pages/test/test.vue
+++ my-vue3-project/src/pages/test/test.vue
@@ -7,15 +7,13 @@
  * 手写测试页面 - 不使用 vite 插件转换
  * 直接内联 defineRenderComponent 的逻辑
  */
-import { ref as vueRef, defineComponent } from 'vue'
-import { render, h, ref } from 'uniapp-render'
-import type { Component } from '@vue/runtime-core'
-import { watch } from '@vue/runtime-core'
+import { h, ref } from 'uniapp-render'
+import {renddd} from "@/pages/test/cccrender";
 
 // ========================================
 // 直接内联 defineRenderComponent 的逻辑
 // ========================================
-export default defineComponent({
+export default renddd({
   setup() {
     console.log('[手写测试] setup called')
 
@@ -30,38 +28,6 @@ export default defineComponent({
       }, '点击 +1')
     ])
     // ========================================
-
-    console.log('[手写测试] got userRenderFn')
-
-    // 2. 包装为组件
-    const InnerComponent: Component = {
-      setup() {
-        console.log('[手写测试] InnerComponent setup called')
-        return userRenderFn
-      }
-    }
-
-    // 3. 使用 render() 获取 RenderNode
-    const nodeInternal = render(InnerComponent)
-    console.log('[手写测试] nodeInternal created:', nodeInternal.value)
-
-    // 4. 创建响应式引用，供模板使用
-    const node = vueRef(nodeInternal.value)
-    console.log('[手写测试] node ref created:', node.value)
-
-    // 5. 监听 nodeInternal 变化，同步到 node
-    watch(
-      () => nodeInternal.value,
-      (newVal: any) => {
-        console.log('[手写测试] nodeInternal changed, updating node')
-        node.value = newVal
-      },
-      { deep: true }
-    )
-
-    // 6. 返回 node 给模板使用
-    console.log('[手写测试] returning { node }')
-    return { node }
   }
 })
 </script>
