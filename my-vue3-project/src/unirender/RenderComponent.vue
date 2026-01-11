01d861ca3b03eb259cbf894d05e50a89817e2ad3
alamhubb <alamhubb@163.com>
2026-01-11 17:38:28 +0800

    1

--- my-vue3-project/src/unirender/RenderComponent.vue
+++ my-vue3-project/src/unirender/RenderComponent.vue
@@ -57,7 +57,8 @@
 </template>
 
 <script lang="ts">
-import { defineComponent, PropType, computed } from 'vue'
+import { defineComponent, computed } from 'vue'
+import type { PropType } from 'vue'
 import { renderEvent } from './event'
 import type { RenderNode } from './types'
 
