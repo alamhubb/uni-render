7128dfbff911d4b5aa1cee4f2435d80b790ee0dc
alamhubb <alamhubb@163.com>
2026-01-11 18:17:09 +0800

    1

--- my-vue3-project/src/pages/index/components/HelloWorld.vue
+++ my-vue3-project/src/pages/index/components/HelloWorld.vue
@@ -1,9 +1,22 @@
-<script setup lang="ts">
-import { ref } from 'vue'
+<script lang="ts">
+import { defineComponent, ref } from 'vue'
 
-defineProps<{ msg: string }>()
+export default defineComponent({
+  name: 'HelloWorld',
+  props: {
+    msg: {
+      type: String,
+      required: true
+    }
+  },
+  setup(props) {
+    const count = ref(0)
 
-const count = ref(0)
+    return {
+      count
+    }
+  }
+})
 </script>
 
 <template>
@@ -19,17 +32,12 @@ const count = ref(0)
 
   <p>
     Check out
-    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-    >create-vue</a
-    >, the official Vue + Vite starter
+    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank">create-vue</a>, the official Vue + Vite
+    starter
   </p>
   <p>
     Learn more about IDE Support for Vue in the
-    <a
-        href="https://vuejs.org/guide/scaling-up/tooling.html#ide-support"
-        target="_blank"
-    >Vue Docs Scaling up Guide</a
-    >.
+    <a href="https://vuejs.org/guide/scaling-up/tooling.html#ide-support" target="_blank">Vue Docs Scaling up Guide</a>.
   </p>
   <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
 </template>
