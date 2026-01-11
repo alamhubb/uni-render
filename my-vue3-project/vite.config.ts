de1d5ab0c3a6f9a64fa7f9e8c003402030cd4915
alamhubb <alamhubb@163.com>
2026-01-12 02:57:49 +0800

    撤回修改必须使用.ts，用.vue的话 uniapp处理后会变为 路径无法渲染

--- my-vue3-project/vite.config.ts
+++ my-vue3-project/vite.config.ts
@@ -14,7 +14,7 @@ export default defineConfig(async ({ mode }) => {
   // 默认 h5 模式
   return {
     plugins: [
-      uniRender({ debug: true }),
+      uniRender(),
       uni()
     ],
     server: {
