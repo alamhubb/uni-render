ee862f836ad1fc83b7d3179636b8ac1fe6e395cd
alamhubb <alamhubb@163.com>
2026-01-11 17:01:36 +0800

    1

--- my-vue3-project/src/unirender/index.ts
+++ my-vue3-project/src/unirender/index.ts
@@ -11,5 +11,9 @@ export { defineRenderComponent } from './defineRenderComponent'
 // 事件系统
 export { renderEvent } from './event'
 
+// 组件
+export { default as RenderComponent } from './RenderComponent.vue'
+
 // 类型定义
 export type { RenderNode } from './types'
+
