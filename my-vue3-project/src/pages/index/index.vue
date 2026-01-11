5f21bcd4546c516458915a2372e330e604a317f8
alamhubb <alamhubb@163.com>
2026-01-11 14:50:03 +0800

    1

--- my-vue3-project/src/pages/index/index.vue
+++ my-vue3-project/src/pages/index/index.vue
@@ -1,23 +1,35 @@
-<script setup lang="ts">
-// 暂时不使用响应式
-</script>
-
 <template>
-  <div>
-    <div>
-      <a href="https://vite.dev" target="_blank">
-        <img src="/static/vite.svg" class="logo" alt="Vite logo" />
-      </a>
-      <a href="https://vuejs.org/" target="_blank">
-        <img src="/static/vue.svg" class="logo vue" alt="Vue logo" />
-      </a>
-    </div>
-    
-    <div style="margin-top: 20px; text-align: center;">
-      <p style="color: #42b883;">Hello UniApp Render!</p>
-    </div>
-  </div>
+  <render-component :node="node" />
 </template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue'
+import { h } from 'uniapp-render'
+
+// 手动编写渲染函数组件
+const MyComponent = defineComponent({
+  setup() {
+    // 返回渲染函数
+    return () => h('div', {}, [
+      h('div', {}, [
+        h('a', { href: 'https://vite.dev', target: '_blank' }, [
+          h('img', { src: '/static/vite.svg', class: 'logo', alt: 'Vite logo' })
+        ]),
+        h('a', { href: 'https://vuejs.org/', target: '_blank' }, [
+          h('img', { src: '/static/vue.svg', class: 'logo vue', alt: 'Vue logo' })
+        ])
+      ]),
+      h('div', { style: 'margin-top: 20px; text-align: center;' }, [
+        h('p', { style: 'color: #42b883;' }, 'Hello UniApp Render!')
+      ])
+    ])
+  }
+})
+
+// 使用 defineRenderComponent 包装
+import { defineRenderComponent } from '@/unirender'
+export default defineRenderComponent(MyComponent)
+</script>
 
 <style>
 .logo {
--- my-vue3-project/vite.config.ts
+++ my-vue3-project/vite.config.ts
@@ -13,7 +13,7 @@ export default defineConfig(async ({ mode }) => {
   // 默认 h5 模式
   return {
     plugins: [
-      uniRender({ debug: true }),  // 启用插件
+      // uniRender({ debug: true }),  // 暂时禁用插件，测试手动写法
       uni()
     ],
     server: {
