b59effa171e75bb4e480f1bdb3757a6720b41309
alamhubb <alamhubb@163.com>
2026-01-12 16:27:31 +0800

    撤回修改必须使用.ts，用.vue的话 uniapp处理后会变为 路径无法渲染

--- my-vue3-project/src/pages/index/components/HelloWorld.vue
+++ my-vue3-project/src/pages/index/components/HelloWorld.vue
@@ -12,7 +12,7 @@ console.log('444')
 </script>
 
 <template>
-  <h1>{{ msg }}</h1>
+  <h1 style="font-size: 2em; color: #333;line-height: 1.1;font-weight: bold;">{{ msg }}</h1>
 
   <div class="card">
     <button type="button" @click="count++">count is {{ count }}</button>
@@ -24,12 +24,16 @@ console.log('444')
 
   <p style="display: flex;flex-direction: row;justify-content: center">
     Check out
-    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank">create-vue</a>, the official Vue + Vite
+    <a style="font-weight: 500;
+    color: #646cff;
+    text-decoration: inherit;" href="https://vuejs.org/guide/quick-start.html#local" target="_blank">create-vue</a>, the official Vue + Vite
     starter
   </p>
   <p style="display: flex;flex-direction: row;justify-content: center">
     Learn more about IDE Support for Vue in the
-    <a href="https://vuejs.org/guide/scaling-up/tooling.html#ide-support" target="_blank">Vue Docs Scaling up Guide</a>.
+    <a style="font-weight: 500;
+    color: #646cff;
+    text-decoration: inherit;" href="https://vuejs.org/guide/scaling-up/tooling.html#ide-support" target="_blank">Vue Docs Scaling up Guide</a>.
   </p>
   <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
 </template>
