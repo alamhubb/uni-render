9f41fa3c8b59855e6c2f284ada666d4fcfea7840
alamhubb <alamhubb@163.com>
2026-01-12 17:23:34 +0800

    撤回修改必须使用.ts，用.vue的话 uniapp处理后会变为 路径无法渲染

--- my-vue3-project/vite.config.ts
+++ my-vue3-project/vite.config.ts
@@ -1,5 +1,5 @@
 import { defineConfig } from "vite";
-import { uniRender } from "vite-plugin-uni-render";
+import { uniRender } from "../vite-plugin-uni-render/dist/index.mjs";
 import { createRequire } from "module";
 import { fileURLToPath } from "url";
 
